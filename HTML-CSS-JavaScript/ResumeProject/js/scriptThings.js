537ba8ae0b66dde54fb7a3730055b95a1d8856d5
Kaia <danielloewito@gmail.com>
2022-10-11 22:12:08 +1100

    I REALLY OVERTHOUGHT IT HUH

--- HTML-CSS-JavaScript/ResumeProject/js/scriptThings.js
+++ HTML-CSS-JavaScript/ResumeProject/js/scriptThings.js
@@ -1,26 +1,47 @@
+// I REALIZE I CAN USE JQUERY BUT THIS IS MY WEBSITE SO I SHOULD DO IT ALL
+// Plus more learning, maybe?
+// I just forgot about it to be honest.
+
+const resumeBtn = document.getElementById("resBtn");
+var resumeState = false;
+resumeBtn.addEventListener("click", function(){
+    imageOpen('../Images/OctResume.jpg', 'resumeAppear', 'resBtn')
+});
+
 function hide(ElementId) {
     var thing = document.getElementById(ElementId);
     thing.style.display = "none";
 }
 
-function imageDisplay(src, givenId, location, buttonId) {
-    var img = document.createElement("img");
-    img.src = src;
-    img.id = givenId;
-    hide(buttonId);
+function show(ElementId) {
+    var thing = document.getElementById(ElementId);
+    thing.style.display = "block"
+}
 
-    document.getElementById(location).appendChild(img);
+function imageOpen(src, location, buttonId) {
+    if (resumeState == false) {
+        var img = "<img src='" + src + "'/>"
+
+        var btn = document.getElementById(buttonId);
+
+        btn.innerHTML = img;
+        btn.id = buttonId;
+
+        resumeState = true;
+
+    } else { 
+        var btn = document.getElementById(buttonId);
+
+        btn.innerHTML = "You've already seen it but up to you??";
+        btn.id = buttonId;
+
+        resumeState = false;
+    }
+    
+
+    document.getElementById(location).appendChild(btn);
 }
 
 function hello() {
     alert("HELLO")
 }
-
-function toggleText() {
-    var text = document.getElementById("resumeAppear");
-    if (text.style.display === "none") {
-      text.style.display = "block";
-    } else {
-      text.style.display = "none";
-    }
-  }
