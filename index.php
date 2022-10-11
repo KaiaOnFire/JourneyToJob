b5b0b5bf39892cae54c1664d3039bd7b83b41e0a
Kaia <danielloewito@gmail.com>
2022-10-12 00:38:14 +1100

    mainPage to index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 
 <?php 
-$sPath = realpath( __DIR__ . '/HTML-CSS-JavaScript-Project/HTML/mainPage.html');
+$sPath = realpath( __DIR__ . '/HTML-CSS-JavaScript-Project/HTML/index.html');
 include_once $sPath;
 ?>
