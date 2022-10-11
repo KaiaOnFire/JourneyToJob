a35cfe0a738eac95aeef4da6e358641d2354205a
Kaia <danielloewito@gmail.com>
2022-10-11 21:08:28 +1100

    Contemplating using an eventlistener but I'm sure I can make this stupid button disappear without it

--- HTML-CSS-JavaScript/ResumeProject/js/scriptThings.js
+++ HTML-CSS-JavaScript/ResumeProject/js/scriptThings.js
@@ -1,3 +1,22 @@
-function hello() {
-    alert('Hello');
+function imageDisplay(src, givenId, location, buttonId) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.id = givenId;
+    alert(buttonId);
+    //getElementById(buttonId).style.visibility="hidden";
+
+    document.getElementById(location).appendChild(img);
 }
+
+function hello() {
+    alert("HELLO")
+}
+
+function toggleText() {
+    var text = document.getElementById("resumeAppear");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
